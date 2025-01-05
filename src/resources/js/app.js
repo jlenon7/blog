30a8b4b3b372603d569d18f1505fa13811f0a51b
jlenon7 <lenonSec7@gmail.com>
2025-01-05 11:29:16 -0300

    chore(npm): update dependencies

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -4,6 +4,13 @@ import feather from 'feather-icons'
 import { articlesComponent } from '../views/pages/articles/index.js'
 import { darkModeComponent } from '../views/components/layout/header/index.js'
 
+/**
+ * Tell vite the existence of other assets then
+ * app.scss and app.js so he could copy everything
+ * to the build directory.
+ */
+import.meta.glob(['../img/**'])
+
 AlpineJS.data('darkModeComponent', darkModeComponent)
 AlpineJS.data('articlesComponent', articlesComponent)
 AlpineJS.start()
